86d534041a7410f65a07b88f5b48708e7a29de0f
TacitusPie <kakmsaikou@gmail.com>
2023-05-17 20:25:56 +0800

    feat: create Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Explore from './pages/Expore';
+import Offer from './pages/Offer';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+
 const App = () => {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offer" element={<Offer />} />
+          <Route path='/profile' element={<SignIn/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/forgot-password' element={<ForgotPassword/>} />
+        </Routes>
+      </Router>
     </>
   );
 };
