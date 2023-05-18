9ef018485eef34c1bcc5588fd4e11aef2c02c835
TacitusPie <kakmsaikou@gmail.com>
2023-05-18 22:43:54 +0800

    feat: import svg with svgr

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Explore from './pages/Expore';
 import Offer from './pages/Offer';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Explore />} />
-          <Route path="/offer" element={<Offer />} />
-          <Route path='/profile' element={<SignIn/>} />
-          <Route path='/sign-in' element={<SignIn/>} />
-          <Route path='/sign-up' element={<SignUp/>} />
-          <Route path='/forgot-password' element={<ForgotPassword/>} />
+          <Route path='/' element={<Explore />} />
+          <Route path='/offer' element={<Offer />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
+        <NavBar />
       </Router>
     </>
   );
